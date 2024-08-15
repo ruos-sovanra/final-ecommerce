09062e6c0f4e0d4fd6dbbabc1b2e5b4c6bae2a79
Ruos Sovanra <vannraruos@gmail.com>
2024-08-15 16:25:07 +0700

    Update Image Size

--- components/collection/CollectionComponent.tsx
+++ components/collection/CollectionComponent.tsx
@@ -57,7 +57,7 @@ const CollectionComponent = () => {
                             <img
                                 src={product.image}
                                 alt={product.name}
-                                className="h-full w-full object-cover object-center cursor-pointer"
+                                className="h-full w-full md:w-[500px] md:h-[500px] object-cover object-center cursor-pointer"
                                 onClick={()=>router.push(`/product/${product.uuid}`)}
                             />
                         </div>
